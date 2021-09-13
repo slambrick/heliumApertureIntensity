8edcddc7250f3acd0fceefaaae7023be33e4074b
Sam Lambrick <sam@DESKTOP-0PECHOF.home>
2021-09-13 08:02:34 +0100

    cone conductance

--- R/coneConductance.R
+++ R/coneConductance.R
@@ -8,7 +8,7 @@
 #' @export
 hole_conductance <- function(ra, rb) {
     helium_factor <- 3.5
-    helium_factor*11.6*pi*(ra*rb/100)
+    helium_factor*11.6*pi*(ra*rb)
 }
 
 #' Aperture conductance
@@ -25,6 +25,31 @@ apertureConductance <- function(beta, working_dist) {
     hole_conductance(r, r)
 }
 
+#' Forwards cone conductance
+#' 
+#' Calculates the forwards conductance of a cone using the formula developed by
+#' B. Mercier (doi.org/10.1116/1.2187996).
+#' 
+#' @param R0 Radius of the smaller opening (mm)
+#' @param Rk Radius of the larger opening (mm)
+#' @param L Length of the cone (mm)
+#' @param alpha Angle of the sides of the cone (deg)
+#' @return The conductance of the aperture in litres per second
+#' @export
+coneForwards <- function(R0, Rk, L, alpha, k) {
+    # TODO: calculate k from the inputs
+    R0 <- R0/1000 # Convert from mm to m
+    Rk <- Rk/1000
+    L <- L/1000
+    kB <- 1.381e-23
+    t <- 300 # Kelvin
+    m_He <- 4*1.66e-27
+    V_m <- sqrt(2*(3/2)*kB*t/m_He)
+    Ca <- (4/3)*pi*V_m*(R0^2*Rk^2*k)/((R0 + Rk)*L)
+    Ca <- Ca*1000 # Convert from m^3/s to litres/s
+    return(Ca)
+}
+
 #' Backwards cone conductance
 #'
 #' Calculates the backwards conductance of a cone using the formula developed by
@@ -36,17 +61,9 @@ apertureConductance <- function(beta, working_dist) {
 #' @param alpha Angle of the sides of the cone (deg)
 #' @return The conductance of the aperture in litres per second
 #' @export
-condeConductance <- function(R0, Rk, L, alpha, k) {
+coneConductance <- function(R0, Rk, L, alpha, k) {
     # TODO: calculate k from the inputs
-    R0 <- R0/1000 # Convert from mm to m
-    Rk <- Rk/1000
-    L <- L/1000
-    kB <- 1.381e-23
-    t <- 300 # Kelvin
-    m_He <- 4*1.66e-27
-    V_m <- sqrt(2*(3/2)*kB*t/m_He)
-    Ca <- (4/3)*pi*V_m*(R0^2*Rk^2*k)/((R0 + Rk)*L)
-    Ca <- Ca*1000
+    Ca <- coneForwards(R0, Rk, L, alpha, k)
     gamma <- 1 + (16/3)*k*tan(alpha*pi/180)
     Cr <- Ca/gamma
     return(Cr)
